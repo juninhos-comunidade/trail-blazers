8a5694f17bb68165feb77311c3ef36417bbba7d9
matheut <matheus.teixeira@tjpe.jus.br>
2026-07-21 12:01:22 -0300

    feat: estrutura inicial dos modulos para autenticação

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { envValidationSchema } from './config/env.validation';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +16,8 @@ import { envValidationSchema } from './config/env.validation';
         abortEarly: false,
       },
     }),
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
