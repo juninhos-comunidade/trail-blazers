1d32929ae8f103a63e23d739b451d70259c0d1b3
matheut <matheus.teixeira@tjpe.jus.br>
2026-07-22 11:09:28 -0300

    feat: estrutura inicial da autenticação

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign-in.dto';
+import { Public } from './auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
+  }
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signIn(request: SignInDto): Promise<{ access_token: string }> {
+    const user = await this.usersService.findOne(request.username);
+    if (user?.password !== request.password) {
+      throw new UnauthorizedException();
+    }
+    const payload = { sub: user.userId, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+
+  async signUp(request: SignUpDto): Promise<any> {
+    //implementar signup
+  }
+}
